f35d59c8e85e45895dcbc10102cae484af4e6661
jakeos987 <jkobw9200@gmail.com>
2025-12-31 10:09:36 +0200

    connection to db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Module, } from '@nestjs/common';
+import {ConfigModule ,ConfigService} from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { UsersModule } from './users/users.module';
+import {TypeOrmModule} from '@nestjs/typeOrm'
+import {User} from './users/entities/user.entity'
 
 @Module({
-  imports: [UserModule, UsersModule],
+  imports: [ UsersModule,ConfigModule.forRoot({
+    isGlobal:true,
+  }),
+TypeOrmModule.forRoot({
+imports:[ConfigModule],
+inject:[ConfigService],
+useFactory:(configService:ConfigService)=>({
+  type:"postgres",
+  host:configService.get<string>('DB_HOST'),
+  port:configService.get<number>('DB_PORT'),
+  username:configService.get<string>('DB_USERNAME'),
+  password:configService.get<string>('DB_PASSWORD'),
+  database:configService.get<string>('DB_DATABACE'),
+  entities:[User],
+  synchronize:true,
+  }),
+}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
